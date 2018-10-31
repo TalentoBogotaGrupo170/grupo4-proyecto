3913e76cd60d8148982bc30648f3856c3fad7f19
Lauro <kinglauro@hotmail.com>
2018-10-31 16:18:43 -0500

    Heroku users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   devise_for :teachers
   get 'home/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_220345) do
+ActiveRecord::Schema.define(version: 2018_10_31_210400) do
 
   create_table "districts", force: :cascade do |t|
     t.string "name"
@@ -86,22 +86,21 @@ ActiveRecord::Schema.define(version: 2018_10_30_220345) do
     t.integer "grade_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["grade_id"], name: "index_teachers_on_grade_id"
+    t.index ["institute_id"], name: "index_teachers_on_institute_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index ["email"], name: "index_teachers_on_email", unique: true
-    t.index ["grade_id"], name: "index_teachers_on_grade_id"
-    t.index ["institute_id"], name: "index_teachers_on_institute_id"
-    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
